85ccca29255b25328dd64074baa2238e17702f02
Jacob Thomsen <jacob.k.t@icloud.com>
2021-09-16 10:11:22 +0200

    rod

---
+++ bauncing ball/bauncingBall.js
@@ -0,0 +1,15 @@
+function setup() {
+    angleMode(DEGREES); // Change the mode to DEGREES
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+    
+    // bolden
+    fill('red');
+    circle(200, 200, 50);
+
+    //bauncing
+    
+}
