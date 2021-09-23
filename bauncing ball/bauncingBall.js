950329136242b5107690e34a0d15e6c2e891bcc9
Jacob Thomsen <jacob.k.t@icloud.com>
2021-09-23 10:05:09 +0200

    ball færdig

--- bauncing ball/bauncingBall.js
+++ bauncing ball/bauncingBall.js
@@ -1,29 +1,27 @@
 function setup() {
-    angleMode(DEGREES); // Change the mode to DEGREES
-    createCanvas(400, 400);
-  }
-  
-  let Ycirkel = 0
-  let speed = 1
-
-  function draw() {
-    background(220);
-    
-
-    // bolden
-    fill('red');
-    circle(200,Ycirkel, 50);
-
-    // bevægelsen
-    Ycirkel = Ycirkel + speed
-
-    if (Ycirkel == 380) {
-      speed = -1
-    }
-
-    if (Ycirkel == 0) {
-      speed = 1
-    }
+  angleMode(DEGREES); // Change the mode to DEGREES
+  createCanvas(windowWidth, windowHeight);
 }
 
- 
+let Ycirkel = 0
+let speed = 3
+function draw() {
+  background(100, mouseX, mouseY);
+
+
+  // bolden
+  fill('red');
+  circle(windowWidth/2, Ycirkel, 150);
+
+  // bevægelsen
+  Ycirkel = Ycirkel +(4*speed)
+
+  if (Ycirkel == windowHeight) {
+    speed = -speed
+  }
+
+  if (Ycirkel == 0) {
+    speed = -speed
+  }
+}
+
