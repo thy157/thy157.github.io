f126e74d7b3d68e6da414356108de6bfaa731915
Jacob Thomsen <jacob.k.t@icloud.com>
2021-09-23 10:17:21 +0200

    blueballs start

---
+++ BlueBalls/bluballs.js
@@ -0,0 +1,14 @@
+function setup() {
+    angleMode(DEGREES); // Change the mode to DEGREES
+    createCanvas(windowWidth, windowHeight);
+  }
+  let xPos = 200
+  let yPos = 200
+  let radius = 5
+  
+  function draw() {
+      
+      background(220);
+      cirkel(xPos,yPos,radius)
+  }
+  
